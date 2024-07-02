f749d1d17fc210efc588a1a61db3e3130701ede6
João Calafange <jotave369@gmail.com>
2024-07-02 09:27:06 -0300

    upload de imagen de perfil funcionando

--- front/lib/components/NomeUsuarioCard.dart
+++ front/lib/components/NomeUsuarioCard.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:front/services/auth_service.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NomeUsuarioCard extends StatefulWidget {
   final void Function(String, String, String) onSubmit;
@@ -14,8 +19,54 @@ class NomeUsuarioCard extends StatefulWidget {
 class _NomeUsuarioCardState extends State<NomeUsuarioCard> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _descricaoController = TextEditingController();
-  final TextEditingController _fotoController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  File? _selectedImage;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _onFinish() async {
+    if (_formKey.currentState!.validate()) {
+      User? user = FirebaseAuth.instance.currentUser;
+
+      if (user != null && _selectedImage != null) {
+        String userId = user.uid;
+        Reference imagemStorageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_profiles/$userId');
+        UploadTask uploadTask = imagemStorageRef.putFile(_selectedImage!);
+        TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+
+        String imageUrl = await taskSnapshot.ref.getDownloadURL();
+
+        FirebaseFirestore firestore = FirebaseFirestore.instance;
+        await firestore.collection('usuarios').doc(userId).update({
+          'username': _usernameController.text.trim(),
+          'descricaoPerfil': _descricaoController.text.trim(),
+          'fotoPerfil': imageUrl,
+          'pontos': 0, // Valor padrão inicial para pontos
+        }).then((_) {
+          widget.onSubmit(
+            _usernameController.text.trim(),
+            _descricaoController.text.trim(),
+            imageUrl,
+          );
+          Navigator.of(context).pop();
+        }).catchError((error) {
+          print("Erro ao atualizar documento: $error");
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,57 +120,45 @@ class _NomeUsuarioCardState extends State<NomeUsuarioCard> {
                 },
               ),
               SizedBox(height: 16),
-              TextFormField(
-                controller: _fotoController,
-                decoration: InputDecoration(
-                  hintText: 'URL da Foto do Perfil',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Por favor, insira a URL da foto do perfil';
-                  }
-                  return null;
-                },
-              ),
+              _selectedImage == null
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () => _pickImage(ImageSource.camera),
+                          child: Text('Tirar Foto'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () => _pickImage(ImageSource.gallery),
+                          child: Text('Escolher da Galeria'),
+                        ),
+                      ],
+                    )
+                  : Image.file(
+                      _selectedImage!,
+                      height: 150,
+                    ),
               SizedBox(height: 16),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ElevatedButton(onPressed:() {
-                    Navigator.of(context).pop();
-                  }, 
-                  child: Text('Cancelar',
-                    style: TextStyle(
-                      color: Colors.red,)
-                  ),
-                  ),
                   ElevatedButton(
                     onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        widget.onSubmit(
-                          _usernameController.text.trim(),
-                          _descricaoController.text.trim(),
-                          _fotoController.text.trim(),
-                        );
-                        context.read<AuthService>().updateUserData(
-                          context.read<AuthService>().usuario!.uid,
-                          username: _usernameController.text.trim(),
-                          descricaoPerfil: _descricaoController.text.trim(),
-                          fotoPerfil: _fotoController.text.trim(),
-                          pontos: 0, // Valor padrão inicial para pontos
-                        ); // Atualiza os dados do usuário no AuthService
-                        Navigator.of(context).pop();
-                      }
+                      Navigator.of(context).pop();
                     },
+                    child: Text(
+                      'Cancelar',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _onFinish,
                     child: Text('Confirmar'),
                   ),
                 ],
               ),
-              
             ],
           ),
         ),
